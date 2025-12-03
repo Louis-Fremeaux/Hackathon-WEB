b9213532f7e65a44a795edb535fe0b6a4d0b6da8
Louis FREMEAUX <louis.fremeaux@icloud.com>
2025-12-03 14:27:48 +0100

    tp1

--- assets/controllers/flip_controller.js
+++ assets/controllers/flip_controller.js
@@ -2,10 +2,12 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
     flip() {
-        this.element.querySelector('.card-inner').classList.add("rotate-y-180");
+        const inner = this.element.querySelector('.card-inner');
+        if (inner) inner.classList.add("rotate-y-180");
     }
 
     unflip() {
-        this.element.querySelector('.card-inner').classList.remove("rotate-y-180");
+        const inner = this.element.querySelector('.card-inner');
+        if (inner) inner.classList.remove("rotate-y-180");
     }
 }
